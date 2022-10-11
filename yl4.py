7a5ee43a24a53c4fa8902cc142ed68b61a095ef5
torumeez <kevinpihlak321@gmail.com>
2022-10-11 13:25:32 +0300

    uus yl tehtud

---
+++ yl4.py
@@ -0,0 +1,12 @@
+a = input('sisesta esimene number:')
+b = input('sisesta teine number:')
+
+
+def answer(a, b):
+      
+    if a <= b:
+        return a
+    else:
+        return b
+
+print(answer(a, b))
